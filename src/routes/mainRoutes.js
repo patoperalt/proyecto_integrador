7bbd740c3db51bdc465650dd9a5f22d1f76daf1e
patoperalta <patricia.peralta@gmail.com>
2023-12-04 14:59:22 -0300

    shop y admin ok

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const mainControllers = require('../controllers/mainController')
 
-router.get('/home', mainControllers.home);
-router.get('/contact', mainControllers.contact);
-router.get('/about', mainControllers.about);
-router.get('/faqs', mainControllers.faqs);
+const {
+  homeView,
+  contactView,
+  aboutView,
+  faqsView,
+} = require('../controllers/mainControllers');
 
+router.get('/', (req, res, next) => {
+
+    req.session.logs = req.session.logs ? ++req.session.logs : 1;
+    console.log(req.session.logs);
+
+    next();
+}, homeView);
+router.get('/contact', contactView);
+router.get('/about', aboutView);
+router.get('/faqs', faqsView);
 
 module.exports = router;
--- src/routes/shopRoutes.js
+++ src/routes/shopRoutes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const {
+    shopView,
+     detailView,
+    addItemToCart,
+    cartView,
+    checkout,
+} = require('../controllers/shopControllers');
 
-router.get('/',(req,res) => res.send('Route for Shop View'));
-router.get('/item/:id', (req,res) => res.send(`Route for find and retrieve a product form the id: ${req.params.id} `));
-router.post('/item/:id/add', (req,res) => res.send('Route for add the current item to the shop cart'));
-router.get('/cart', (req,res) => res.send('Route for cart view'));
-router.post('/cart', (req,res) => res.send('Route for go to checkout View'));
+router.get('/', shopView);
+router.get('/item/:id', detailView);
+router.post('/item/:id/add', addItemToCart);
+router.get('/cart', cartView);
+router.post('/cart', checkout);
 
 module.exports = router;
 
