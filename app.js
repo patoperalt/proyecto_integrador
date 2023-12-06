63a0428b30354a166425844f70d9db9c7610ed99
patoperalta <patricia.peralta@gmail.com>
2023-12-06 18:09:48 -0300

    [FEAT] methd overrides and body parsers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override')
 
 const mainRoutes = require('./src/routes/mainRoutes');
 const shopRoutes = require('./src/routes/shopRoutes');
@@ -7,10 +8,13 @@ const adminRoutes = require('./src/routes/adminRoutes');
 const authRoutes = require('./src/routes/authRoutes');
 
 
-
 const PORT = 4000;
 
 app.use(express.static('public'));
+
+app.use(express.urlencoded()); 
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
